7468cedd5e1c18a0b366dc01676670ef059a196a
shinjeha <shinjeha1993@gmail.com>
2025-06-24 16:48:12 +0900

    테스트 커밋

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Value("${app.version:1.0.0}")
+    String version;
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Web Page!. " + version;
+    }
+
+}
